d017d41f4cde17adfd7915cf91f1a78936d0d08a
PAAWSA02LC0904 <Duoc@PAAWSA02LC0904.duoc.acad>
2025-11-04 22:04:26 -0300

    primera ruta

---
+++ src/pages.js/home.js
@@ -0,0 +1,26 @@
+import React from "react";
+import NavbarPrincipal from "./components/navbar";
+
+function HomePrincipal (){
+    return (
+
+    <div>
+        <NavbarPrincipal/>
+        <div className="row">
+            <div className= 'col-md-12'>
+            <h2>Pagina Principal</h2>
+            <p>jhsuahdksudksadusauksxnksaxnjsnxka
+                slajsljsalkjslasjlaksjaksjassa 
+                scinscnzsncsjknjkzsnckjscnzjk
+                jhsuahdksudksadusauksxnksaxnjsnxka </p>
+
+
+            </div>
+        </div>
+        </div>
+
+    );
+}
+
+
+export default HomePrincipal;
---
+++ src/pages.js/products.js
@@ -0,0 +1,34 @@
+import React from "react";
+import NavbarPrincipal from "./components/navbar"
+
+function ProductsPrincipal (){
+    return (
+        
+    <div>
+        <NavbarPrincipal/>
+        <div className="row">
+            <div className= 'col-md-12'>
+            <h2>Productos Destacados</h2>
+            <p>jhsuahdksudksadusauksxnksaxnjsnxka
+                slajsljsalkjslasjlaksjaksjassa 
+                scinscnzsncsjknjkzsnckjscnzjk
+                jhsuahdksudksadusauksxnksaxnjsnxka
+                slajsljsalkjslasjlaksjaksjassa 
+                scinscnzsncsjknjkzsnckjscnzjkjh
+                suahdksudksadusauksxnksaxnjsnxka
+                slajsljsalkjslasjlaksjaksjassa 
+                scinscnzsncsjknjkzsnckjscnzjkjhsu
+                ahdksudksadusauksxnksaxnjsnxka
+                slajsljsalkjslasjlaksjaksjassa 
+                scinscnzsncsjknjkzsnckjscnzjk </p>
+
+
+            </div>
+        </div>
+    </div>
+
+    );
+}
+
+
+export default ProductsPrincipal;
