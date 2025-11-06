a21dbe6110cfa42fa06ac98c195f52afe461884b
PAAWSA02LC0904 <Duoc@PAAWSA02LC0904.duoc.acad>
2025-11-05 22:06:19 -0300

    Avances clase 05/11

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePrincipal from "./pages.js/home";
-import ProductsPrincipal from "./pages.js/products";
+import HomePrincipal from "./pages/home";
+import ProductsPrincipal from "./pages/products";
+import ListaProducto from "./pages/product_list";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePrincipal/>}></Route>
         <Route path='/products' element={<ProductsPrincipal/>}></Route>
+        <Route path='/lista-productos' element={<ListaProducto/>}></Route>
+
       </Routes>
     </Router>
   );
